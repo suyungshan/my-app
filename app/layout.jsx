e11b48203a81fbd31cc53972e91eb1858f84ad86
suyungshan <samsys2324@gmail.com>
2024-07-06 01:20:03 +0800

    禁止放大

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,9 +11,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta content="yes" name="apple-mobile-web-app-capable" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
       </head>
       <StoreProvider>
