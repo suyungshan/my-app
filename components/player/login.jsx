3886a323a4b1b8e1f17fbc5bab9618a5ae7b7312
suyungshan <samsys2324@gmail.com>
2024-07-10 22:53:29 +0800

    調整好首頁

--- components/player/login.jsx
+++ components/player/login.jsx
@@ -16,7 +16,7 @@ export default function Login() {
     event.preventDefault();
     const nickName = inputValue.trim();
 
-    if (nickName === "2920") {
+    if (nickName === "0807") {
       router.push("/player/enterName");
     } else {
       setInputValue("遊戲尚未開始，敬請期待");
@@ -45,10 +45,10 @@ export default function Login() {
   return (
     <div className="flex justify-start flex-col items-center gap-[1%] w-full h-full px-[5%] py-[2%]">
       <form
-        className="flex w-full h-full justify-center items-center flex-col gap-[32%] rounded-[3vw] relative"
+        className="flex  w-full h-full justify-center items-center flex-col gap-[5%] rounded-[3vw] relative"
         onSubmit={submitHandler}
       >
-        <div className="relative w-[80%] h-[20%] mt-[-5%]">
+        <div className="relative w-[80%] h-[10%] mt-[-5%]">
           <input
             ref={inputRef}
             type="text"
@@ -60,7 +60,7 @@ export default function Login() {
             onChange={handleInputChange}
             onFocus={handleInputFocus}
             className={`
-              w-full h-full px-[3%] py-[2%] rounded-[3vw]
+              w-full h-full border-2 border-[#fff]  px-[6%] py-[2%] rounded-[3vw]
                bg-[#f0f0f0]
               ${
                 hasError || repeatedName
@@ -70,16 +70,16 @@ export default function Login() {
               focus:outline-none
               transition-all duration-300 ease-in-out
             `}
+            style={{ boxShadow: "inset 0 2px 4px 0 rgba(0, 0, 0, 0.1)" }}
           />
         </div>
         <button
           type="submit"
-          className="border-2 px-[8%] border-[#fff] bg-[#647fed] text-[1.5vw] font-[500] text-white rounded-[0.5vw] py-[1%]"
+          className="border-2 w-[40%] h-[7%] px-[8%] border-[#fff]  bg-[#f0f0f0a3] text-[4vw] font-[500] text-white rounded-[3vw] py-[1%]"
           style={{
             color: "black",
-            backgroundColor: "rgba(240, 240, 240, 0.5)", // 微透明的背景
-            boxShadow: "inset 0 2px 4px 0 rgba(0, 0, 0, 0.1)",
-            border: "2px solid #800080",
+            // 微透明的背景
+            boxShadow: " 0 2px 4px 0 rgba(0, 0, 0, 0.1)",
           }}
         >
           確定
